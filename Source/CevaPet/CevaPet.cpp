a65c8c9c1c920706f77b3d9d90f9b3812cf0a43e
Luiz Augusto Melo <luiz@twistereventos.com>
2018-05-08 19:17:03 -0300

    Initial commit, clear project

---
+++ Source/CevaPet/CevaPet.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CevaPet.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CevaPet, "CevaPet" );
